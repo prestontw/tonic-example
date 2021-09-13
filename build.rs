0787f11b00b7118ec19ba14f3e7d0030f07d7170
Preston TW <preston@commure.com>
2021-09-13 18:20:27 -0400

    Try adding http support
    Requires having access to https://github.com/googleapis/googleapis

--- build.rs
+++ build.rs
@@ -1,5 +1,12 @@
-fn main() {
+use std::env::{self, VarError};
+
+fn main() -> Result<(), VarError> {
+    let mut includes = vec!["proto".to_string()];
+    let dir = env::var("GOOGLEAPIS_DIR")?;
+    includes.push(dir);
+
     tonic_build::configure()
-        .compile(&["proto/echo.proto"], &["proto"])
+        .compile(&["proto/echo.proto".to_string()], &includes)
         .unwrap();
+    Ok(())
 }
